bd8b1438f82e8e50addf7290f9a3102ee19b311c
NightSoul <w1083670160@163.com>
2024-08-03 03:01:20 +0800

    ADD:MY Love Video

--- src/web/Test.vue
+++ src/web/Test.vue
@@ -1,21 +1,31 @@
+<template>
+    <Son :a="a" @setA="setA"></Son>
+</template>
+
 <script setup lang='ts'>
-import { ref, watch } from 'vue'
-const a = ref(1)
-const test  = ()=> {
-    a.value++
+const a = ref(0)
+
+function setA(value:string) {
+    a.value = value
 }
 
-watch(()=>a,(a,b)=> {
-    console.log('[ a,b ] >', a,b)
+interface IEmit {
+    (e: 'setA', value: string): void
+}
 
-},{deep:true})
+const emit = defineEmits<IEmit>()
+emit('setA', 2)
 </script>
 
 
-<template>
-    <button @click="test">{{ a }}</button>
-</template>
 
-<style  scoped>
 
-</style>
+
+
+
+// 只有defineProps<IProps>()的时候 <template> 中可以直接使用 name
+// 但是在 script 中需要变量来接收
+// const props = defineProps<IProps>()
+// props.name => 这样 JS 或者 TS 才可以获取到值
+// 需要注意的是不能使用结构赋值,否则失去响应式可以使用 const { name } = toRefs(props) // 此时可以结构赋值
+
