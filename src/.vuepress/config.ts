108646e7395f4e8aab81fafe5c954a459ee16bf3
Rocco Koury <83497094+NightSquirrl@users.noreply.github.com>
2024-11-04 17:23:21 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { commentPlugin } from "vuepress-plugin-comment2";
 export default defineUserConfig({
-  base: "/",
+  base: "./",
 
   lang: "zh-CN",
   title: "Night.Soul",
